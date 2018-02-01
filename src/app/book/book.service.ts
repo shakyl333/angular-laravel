b8cc234ac0ff4bd9a8803ab603614f21bac7ac5b
shakyl.mansuri@gmail.com <shakyl.mansuri@gmail.com>
2018-02-01 13:11:51 +0530

    Found an error in src/app/book/book.service.ts(4,26) Cannot find module 'rxjs/observable'.
    make observable as Observable, o is capital in declaration please correct this

--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
-import {Observable} from 'rxjs/observable';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 import {Book} from './book';
